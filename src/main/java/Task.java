90feb8ac26f59af93d24c9e5877af41674cf625c
Nhat Hoang <hoangnh092185@gmail.com>
2016-09-19 21:51:49 -0700

    add methods to task and include route to app and test java file.

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -72,5 +72,14 @@ public class Task {
              this.getId() == newTask.getCategoryId();
     }
   }
+  public void update(String description) {
+  try(Connection con = DB.sql2o.open()) {
+    String sql = "UPDATE tasks SET description = :description WHERE id = :id";
+    con.createQuery(sql)
+      .addParameter("description", description)
+      .addParameter("id", id)
+      .executeUpdate();
+    }
+  }
 
 }
